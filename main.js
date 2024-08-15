836f6623eb28754d566eff2fba3d64ebf351c0af
ku-ryu <kubo.ryu.826@gmail.com>
2024-08-15 23:16:14 +0900

    day1-2

--- main.js
+++ main.js
@@ -31,7 +31,13 @@
 
 //constで定義した配列はプロパティの変更が可能
 
-const val5 = ["dog", "cat"];
-val5[0] = "bird";
-val5.push("monkey");
-console.log(val5);
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+const name = "kryu"
+const age = 20
+
+const message = `私の名前は${name}です。年齢は${age}`
+console.log(message)

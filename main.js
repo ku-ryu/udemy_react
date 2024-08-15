e243b13cdc9f7ff7cbf02726751acbccfc8a5303
ku-ryu <kubo.ryu.826@gmail.com>
2024-08-15 19:49:14 +0900

    day1

--- main.js
+++ main.js
@@ -1,1 +1,37 @@
-console.log('hoge');
+// var val1 = "var";
+// console.log(val1);
+
+// // var変数は上書き可能
+// val1 = "var変数上書き";
+// console.log(val1);
+
+// //var変数は再宣言可能
+// var val1 = "var変数再宣言";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(vel2);
+
+// // letは再宣言不可能
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // constは上書きも不可能
+
+// const val4 = {
+//     name: "kryu",
+//     age : 24
+// };
+// console.log(val4);
+
+// val4.name = "k0ryu";
+// val4.address = "Gifu";
+// console.log(val4);
+
+//constで定義した配列はプロパティの変更が可能
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);

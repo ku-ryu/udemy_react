3e97053bef97cc24407fe762bf4de5e42a0c7d2d
ku-ryu <kubo.ryu.826@gmail.com>
2024-08-17 23:33:28 +0900

    day2

--- main.js
+++ main.js
@@ -45,5 +45,27 @@
 // const func = str => str
 // console.log(func("funcvv"))
 
-const func = str => `I am ${str}`
-console.log(func("kryu"))
+// const func = str => `I am ${str}`
+// console.log(func("kryu"))
+
+// const myProfile = {
+//     name : "kryu",
+//     age : 24
+// }
+
+// const message1 = `名前は${myProfile.name}です。年齢は${myProfile.age}です。`
+// console.log(message1)
+
+// const {name, age} = myProfile
+// const message2 = `名前は${name}です。年齢は${age}です。`
+// console.log(message2)
+
+// const myProfile = ["kryu", 24]
+
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です`
+// console.log(message3)
+
+// const [name, age] = myProfile
+
+// const message4 = `名前は${name}です。年齢は${age}です`
+// console.log(message4)

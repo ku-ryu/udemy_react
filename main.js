c77bef47bc7fa166fac073968dfc016ae9a29af9
ku-ryu <kubo.ryu.826@gmail.com>
2024-08-19 03:24:07 +0900

    day4

--- main.js
+++ main.js
@@ -82,3 +82,27 @@
 // console.log(age)
 // console.log(name)
 
+// const name = 'kryu'
+// const age = 24
+
+// const myProfile = { name, age }
+// console.log(myProfile)
+
+// const arr1 = [1, 2]
+
+// const sumFunc = (num1, num2) => console.log(num1 + num2)
+// // sumFunc(arr1[0], arr1[1])
+// sumFunc(...arr1)
+
+// const arr2 = [1, 2, 3, 4, 5]
+// const [num1, num2, ...arr3] = arr2
+// console.log(num1)
+// console.log(num2)
+// console.log(arr3)
+
+const arr4 = [10, 20]
+const arr5 = [30, 40]
+
+const arr6 = [...arr4]
+const arr7 = [...arr4, ...arr5]
+console.log(arr7)
